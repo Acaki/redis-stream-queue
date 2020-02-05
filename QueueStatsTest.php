f874a1bfbccef6014e4ed8b8698cae517e65d767
Acaki <axsz4298@gmail.com>
2020-02-05 18:14:02 +0800

    Add tests

---
+++ QueueStatsTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace RedisStreamQueue;
+
+use PHPUnit\Framework\TestCase;
+
+class QueueStatsTest extends TestCase
+{
+    public static function setUpBeforeClass()
+    {
+        Redis::setConnInfo(getenv('REDIS_HOST'), getenv('REDIS_PASS'));
+    }
+
+    public function testGetJobs()
+    {
+        $jobs = QueueStats::getJobs(getenv('REDIS_KEY'), 100);
+        $this->assertLessThanOrEqual(100, count($jobs));
+        $this->assertIsArray($jobs);
+    }
+
+    public function testGetJobCount()
+    {
+        $jobCount = QueueStats::getJobCount(getenv('REDIS_KEY'));
+        $this->assertIsInt($jobCount);
+    }
+
+    public function testGetFirstJob()
+    {
+        $job = QueueStats::getFirstJob(getenv('REDIS_KEY'));
+        $this->assertIsArray($job);
+    }
+
+    public function testGetLastJob()
+    {
+        $job = QueueStats::getLastJob(getenv('REDIS_KEY'));
+        $this->assertIsArray($job);
+    }
+
+    public function testGetPendingJobs()
+    {
+        $pendingJobs = QueueStats::getPendingJobs(getenv('REDIS_KEY'), getenv('REDIS_GROUP'), 1000);
+        $this->assertIsArray($pendingJobs);
+        $this->assertLessThanOrEqual(1000, count($pendingJobs));
+    }
+
+    public function testGetPendingCount()
+    {
+        $count = QueueStats::getPendingCount(getenv('REDIS_KEY'), getenv('REDIS_GROUP'));
+        $this->assertIsInt($count);
+    }
+}
